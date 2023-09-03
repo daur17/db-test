45f67dcfc884725712bfd90430c878965144ceaf
DAUR <douglas.alejandro17@gmail.com>
2023-09-02 19:22:18 -0600

    add (docker): postgresql container

---
+++ data/premier_league.match.sql
@@ -0,0 +1,23 @@
+-- Table: premier_league.match
+
+-- DROP TABLE IF EXISTS premier_league.match;
+
+CREATE TABLE IF NOT EXISTS premier_league.match
+(
+    week integer,
+    date date,
+    "time" text COLLATE pg_catalog."default",
+    home_team text COLLATE pg_catalog."default",
+    home_xg numeric,
+    score text COLLATE pg_catalog."default",
+    away_xg numeric,
+    away_team text COLLATE pg_catalog."default",
+    attendance numeric,
+    venue text COLLATE pg_catalog."default",
+    referee text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS premier_league.match
+    OWNER to postgres;
