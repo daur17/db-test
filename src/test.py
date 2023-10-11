5424be6a71bab3a944496809524a84ced37f141a
DAUR <dumana@eaglehubs.com>
2023-10-11 14:10:06 -0600

    update(github): workflow test

--- src/test.py
+++ src/test.py
@@ -1,3 +1,3 @@
 import os
 
-print("esto es una prueba...", os.getenv("DATABRICKS_CLIENT_ID"), os.getenv("DATBRICKS_CLIENT_SECRET"))
+print("esto es una prueba...", os.getenv("DATABRICKS_CLIENT_ID"), os.getenv("DATABRICKS_CLIENT_SECRET"))
